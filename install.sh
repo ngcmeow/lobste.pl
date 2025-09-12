e5468ea5c38d2a3c394a932d674acad9ed47d4b4
Troy <230756235+ngcmeow@users.noreply.github.com>
2025-09-12 18:54:19 +0100

    Codeberg migration

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+BASEDIR=$(dirname $0)
+ZSHRC=~/.zshrc
+BASHRC=~/.bashrc
+ZSH_PATH=/usr/bin/zsh
+BASH_PATH=/bin/bash
+
+echo "ALWAYS REVIEW SCRIPTS BEFORE RUNNING!"
+echo "----------------------------------------"
+echo ""
+
+sleep 10
+
+mkdir -p ~/bin > /dev/null 2>&1
+cp -v $BASEDIR/src/headlines.pl ~/bin
+
+if [ "$SHELL" = "/usr/bin/zsh" ]; then
+    cat $BASEDIR/src/lobsters_startup.sh >> $ZSHRC
+fi
+
+if [ "$SHELL" = $BASH_PATH ]; then
+    cat $BASEDIR/src/lobsters_startup.sh >> $BASHRC
+fi
